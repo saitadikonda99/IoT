d95404b36e5948d553ed02707d2e64bdf350001c
saitadikonda99 <saiitadikonda7@gmail.com>
2023-01-28 16:00:19 +0530

    exp4

---
+++ exp4.cpp
@@ -0,0 +1,31 @@
+int redled = 3;
+int greenled = 2;
+int buz =4;
+int Sensor = A0;
+
+void setup() {
+
+        pinMode( redled, OUTPUT );
+        pinMode( greenled, OUTPUT );
+        pinMode( buz, OUTPUT );
+        pinMode( Sensor, INPUT );
+        Serial.begin(9600);
+}
+
+void loop() {
+
+        int sensValue = analogRead( Sensor );
+        Serial.println(sensValue);
+  if( sensValue > 150 ) {
+
+        digitalWrite( redled, HIGH );
+        tone( buz, 10000, 10);
+        digitalWrite( greenled, LOW );
+        }
+        else {
+        digitalWrite( greenled, LOW );
+        noTone( buz );
+        digitalWrite( redled, LOW );
+        }
+   }
+
