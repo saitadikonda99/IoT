3764e0a428921da2d36ae9f45d199783a79fb449
saitadikonda99 <saiitadikonda7@gmail.com>
2023-02-25 21:29:27 +0530

    update from sai

--- exp5.cpp
+++ exp5.cpp
@@ -1,19 +1,17 @@
-int buttonState=0;
-void setup()
-{
-  pinMode(2,INPUT);
-  pinMode(LED_BUILTIN,OUTPUT);
+// Define the input pin where the PIR sensor is connected
+int pirPin = 2;
+
+void setup() {
+  pinMode(pirPin, INPUT);  // Set the PIR pin as an input
+  Serial.begin(9600);      // Initialize the serial communication
 }
-void loop()
-{
-  buttonState=digitalRead(2);
-  if(buttonState==HIGH)
-  {
-    digitalWrite(LED_BUILTIN,HIGH);
+
+void loop() {
+  int pirValue = digitalRead(pirPin);  // Read the PIR sensor value
+
+  if (pirValue == HIGH) {  // If motion is detected
+    Serial.println("Motion detected!");
+    delay(1000);           // Wait for 1 second before checking again
   }
-  else
-  {
-    digitalWrite(LED_BUILTIN,LOW);
-  }
-  delay(5);
 }
+
