75e5e5709da454d214c5a6399ef7f75e00d2080f
saitadikonda99 <saiitadikonda7@gmail.com>
2023-02-26 12:44:12 +0530

    update from sai

--- exp2.cpp
+++ exp2.cpp
@@ -1,14 +1,18 @@
-// C++ code
+ int ledPin = 13; // Define LED pin
+int buttonPin = 2; // Define button pin
+int buttonState = 0; // Define button state
 
-void setup()
-{
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
+  pinMode(ledPin, OUTPUT); // Set LED pin as output
+  pinMode(buttonPin, INPUT); // Set button pin as input
 }
 
-void loop()
-{
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+void loop() {
+  buttonState = digitalRead(buttonPin); // Read the state of the button
+  if (buttonState == HIGH) { // If button is pressed
+    digitalWrite(ledPin, HIGH); // Turn on the LED
+  } else { // If button is not pressed
+    digitalWrite(ledPin, LOW); // Turn off the LED
+  }
 }
+
